92323609aefb5a798eafee0cd67aaf3cef43830a
Kunal Kumar <chaudharykunal581@gmal.com>
2023-09-15 22:50:50 +0530

    Level 4 - Salting & Hashing Passwords with Bcrypt

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import md5 from "md5";
+import bcrypt from "bcrypt";
 
 const app = express();
+const saltRounds = 12;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended : true }));
@@ -32,27 +33,31 @@ app.get("/register",(req,res)=>{
 });
 
 app.post("/register",(req,res)=>{
-    const newUser = new User({
-        email : req.body["username"],
-        password : md5(req.body["password"]),
-    });
 
-    newUser.save().then(()=>{
-        res.render("secrets");
-    }).catch((err)=>{
-        console.log(err);
+    bcrypt.hash(req.body["password"],saltRounds,(err,hash)=>{
+        const newUser = new User({
+            email : req.body["username"],
+            password : hash,
+        });
+        newUser.save().then(()=>{
+            res.render("secrets");
+        }).catch((err)=>{
+            console.log(err);
+        });
     });
 });
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email : username }).then((foundUser)=>{
         if(foundUser) {
-            if(foundUser.password === password) {
-                res.render("secrets");
-            }
+            bcrypt.compare(password,foundUser.password).then((result)=>{
+                if(result === true){
+                    res.render("secrets");
+                }
+            });
         }
     }).catch((err)=>{
         console.log(err);
